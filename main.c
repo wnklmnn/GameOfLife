d98c69be7f1f3e4e98a9ab9174a6c0554b14707d
Pascal Winkelmann <pascal@wnklmnn.de>
2015-06-01 19:35:11 +0200

    Fps begrenzung fuer Linux eingebaut.

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <time.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include "util.h"
 #include "drawing.h"
 #include "gol.h"
@@ -13,11 +12,16 @@
 #include "vendor/pdcurses/curses.h"
 #else
 #include <curses.h>
+#include <sys/time.h>
 #endif
 
 int main(int argc, char** argv){
+    #ifdef WIN32
+    //WindowsImplementation
+    #else
     struct timeval tv;
     double tmpTime1, tmpTime2;
+    #endif
     double timePerFrame;
     srand(time(NULL));
     GameOfLife GoL;
@@ -47,8 +51,14 @@ int main(int argc, char** argv){
     }
 
     while(1){
+        
+        #ifdef WIN32
+        //WindowsImplementation
+        #else
+        //Linux Implementation
         gettimeofday(&tv, NULL);
         tmpTime1 = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000; 
+        #endif
         CalcIteration(&GoL);
         AusgabeSpielfeld(GoL, subWin);
         updateHeadWin(headWin, GoL);
@@ -58,10 +68,15 @@ int main(int argc, char** argv){
             }
             getchar();
         }else {
+        #ifdef WIN32
+        //WindowsImplementation
+        #else
+        //Linux Implementation
         do{
             gettimeofday(&tv, NULL);
             tmpTime2 = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000 ;
             }while(tmpTime2 < tmpTime1+timePerFrame);
+        #endif
         }
     }
 
